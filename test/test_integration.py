3db19df592d95eb347cdda2862208a63d0545f8b
Emma <EmmaRussellKilburn@gmail.com>
2024-08-22 10:23:43 +0100

    add integration test

---
+++ test/test_integration.py
@@ -0,0 +1,43 @@
+import json
+import requests
+import os
+
+import constellation.docker_util as docker_util
+
+from src import daedalus_config
+from src import daedalus_constellation
+
+
+def test_start_daedalus():
+    # use a config that doesn't involve vault secrets
+    cfg = daedalus_config.DaedalusConfig("config", "fakeproxy")
+    constellation = daedalus_constellation.DaedalusConstellation(cfg, False)
+    constellation.obj.status()
+    constellation.start()
+
+    assert docker_util.network_exists("daedalus")
+    assert docker_util.volume_exists("daedalus-data")
+    assert docker_util.volume_exists("proxy-logs")
+    assert docker_util.container_exists("daedalus-api")
+    assert docker_util.container_exists("daedalus-web-app-db")
+    assert docker_util.container_exists("daedalus-web-app")
+    assert docker_util.container_exists("daedalus-proxy")
+
+    # ignore SSL
+    session = requests.Session()
+    session.verify = False
+    session.trust_env = False
+    os.environ['CURL_CA_BUNDLE'] = ""
+    res = session.get("https://localhost", verify=False)
+
+    assert res.status_code == 200
+
+    obj.destroy()
+
+    assert not docker_util.network_exists("daedalus")
+    assert not docker_util.volume_exists("daedalus-data")
+    assert not docker_util.volume_exists("proxy-logs")
+    assert not docker_util.container_exists("daedalus-api")
+    assert not docker_util.container_exists("daedalus-web-app-db")
+    assert not docker_util.container_exists("daedalus-web-app")
+    assert not docker_util.container_exists("daedalus-proxy")
