f3c2a3793335eaab8ed7b1ec777723b86a1e8f4c
Emma <EmmaRussellKilburn@gmail.com>
2025-01-09 11:26:55 +0000

    give the worker some time to start

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,7 +1,18 @@
 from constellation import docker_util
+import math
 
 from src.daedalus_deploy.config import DaedalusConfig
 from src.daedalus_deploy.daedalus_constellation import DaedalusConstellation
+
+def wait_for_container_matching(prefix, stopped, poll=0.1, timeout=5):
+    found = False
+    for i in range(math.ceil(timeout / poll)):
+        if len(docker_util.containers_matching(prefix, stopped)) > 0:
+            found = True
+            break
+        time.sleep(poll)
+        container.reload()
+    return found
 
 
 def test_start_daedalus():
@@ -18,7 +29,7 @@ def test_start_daedalus():
     assert docker_util.volume_exists("daedalus-model-results")
     assert docker_util.container_exists("daedalus-redis")
     assert docker_util.container_exists("daedalus-api")
-    assert len(docker_util.containers_matching("daedalus-api-worker", False)) == 1
+    assert wait_for_container_matching("daedalus-api-worker", False)
     assert docker_util.container_exists("daedalus-web-app-db")
     assert docker_util.container_exists("daedalus-web-app")
     assert docker_util.container_exists("daedalus-proxy")
